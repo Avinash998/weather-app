cdf9bd4838eeab29216bc674682a6d6a9b4ad3a9
avinash <avinashgolu.chourasia@gmail.com>
2018-09-28 16:27:30 +0530

    Modifying geolocation api services

--- app-promise.js
+++ app-promise.js
@@ -19,18 +19,18 @@ const argv = yargs
 			.argv;
 
 	const encodedAddress = encodeURIComponent(argv.address);
-	const encodedURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-
+	// const encodedURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+	const encodedURL = `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=HKEoyGBzCrMkIc9zFgMS&app_code=nUqh_eMcXRGh4mWRxxpiYw&gen=9`;
 	axios.get(encodedURL).then((response) => {
 
-		if(response.data.status === 'ZERO_RESULTS'){
+		if(response.data.Response.View.length === 0){
 			throw new Error('Unable to find the address...');
 		}
 
-		let lat = response.data.results[0].geometry.location.lat;
-		let lng = response.data.results[0].geometry.location.lng;
+		let lat = response.data.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+		let lng = response.data.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
 		let weatherURL = `https://api.darksky.net/forecast/4e92265d4c9bd703ef1a713cbf7ad64f/${lat},${lng}`;
-		console.log(response.data.results[0].formatted_address);
+		console.log(response.data.Response.View[0].Result[0].Location.Address.Label);
 		
 		return axios.get(weatherURL);
 
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -4,27 +4,25 @@ const request = require('request');
 const geocodeAddress = (address , callback) => {
 
 	const encodedAddress = encodeURIComponent(address);
-	const encodedURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+	const encodedURL = `https://geocoder.api.here.com/6.2/geocode.json?searchtext=${encodedAddress}&app_id=HKEoyGBzCrMkIc9zFgMS&app_code=nUqh_eMcXRGh4mWRxxpiYw&gen=9`;
 	console.log(encodedURL);
 
 	request({
 		url: encodedURL,
 		json: true
 	}, function (error,response,body){
-		// console.log(JSON.stringify(body,undefined,2));
-		// console.log(JSON.stringify(response.body,undefined,2));
-		//console.log(JSON.stringify(error,undefined,2));
+
 		if(error){
 			callback(`Unable to find Google Servers , Error message:  ${error}`);
 		}
-		else if(body.status === 'ZERO_RESULTS'){
+		else if(response.body.Response.View.length === 0){
 			callback('Unable to find the address.');
 		}
-		else if(body.status === 'OK'){
+		else if(response.body.Response.View.length !== 0){
 			callback(undefined, {
-				address: body.results[0].formatted_address,
-				latitude: body.results[0].geometry.location.lat,
-				longitude: body.results[0].geometry.location.lng
+				address: response.body.Response.View[0].Result[0].Location.Address.Label,
+				latitude: response.body.Response.View[0].Result[0].Location.DisplayPosition.Latitude,
+				longitude: response.body.Response.View[0].Result[0].Location.DisplayPosition.Longitude
 
 			});
 		}
